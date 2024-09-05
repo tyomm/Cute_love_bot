c1c248d770d2d383a8571d845838c2d18cc55ded
Artyom Sarkisyan <97842453+tyomm@users.noreply.github.com>
2024-09-05 23:20:13 +0400

    Update main.py

--- code/main.py
+++ code/main.py
@@ -46,7 +46,8 @@ def start_message(message):
   button14 = types.KeyboardButton('/me?')
   button15 = types.KeyboardButton('/i_am_sad_now')
   button16 = types.KeyboardButton('/our_song')
-  keyboard.add(button1, button2, button3, button4, button5, button6,   button7, button8, button9, button10, button11,
+  button17 = types.KeyboardButton('/test_button')
+  keyboard.add(button17, button1, button2, button3, button4, button5, button6,   button7, button8, button9, button10, button11,
                button12, button13, button14, button15, button16)
 
   bot.send_message(message.chat.id, 'Press the buttons as much as you want😁', reply_markup=keyboard)
