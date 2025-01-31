e33dbfa056ca73917a8863152d57d4a99a6515f6
Artyom Sarkisyan <97842453+tyomm@users.noreply.github.com>
2025-01-31 22:00:32 +0400

    Update main.py

--- code/main.py
+++ code/main.py
@@ -50,6 +50,8 @@ def start_message(message):
                button12, button13, button14, button15, button16)
 
   bot.send_message(message.chat.id, 'Press the buttons as much as you want😁', reply_markup=keyboard)
+  
+bot.polling(none_stop=True)
 #====================0 /start COMMAND 0=========================
 
 
@@ -460,34 +462,34 @@ def start_message(message):
 
 
 
-#==============1 Save Telegram DataBase 1================
-closed = False
+# #==============1 Save Telegram DataBase 1================
+# closed = False
 
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
+# @bot.message_handler(content_types=['text'])
+# def get_text_messages(message):
 
-  global closed
-  try:
-    if message.text.lower() == '/':  # '/finish'
-      print(message.text)
-      closed = True
-      bot.send_message(6921647429, "Closed the DataBase   /")
-      with open("Data_Base/File_DataBase.txt", "rb") as file1:
-        bot.send_document(6921647429, file1)
-      with open("Data_Base/File_DataBase.txt", "w") as file:  # for delete file content
-        file.truncate()  # for delete file content
-  except:
-    print("huzich a")
+#   global closed
+#   try:
+#     if message.text.lower() == '/':  # '/finish'
+#       print(message.text)
+#       closed = True
+#       bot.send_message(6921647429, "Closed the DataBase   /")
+#       with open("Data_Base/File_DataBase.txt", "rb") as file1:
+#         bot.send_document(6921647429, file1)
+#       with open("Data_Base/File_DataBase.txt", "w") as file:  # for delete file content
+#         file.truncate()  # for delete file content
+#   except:
+#     print("huzich a")
     
-  if message.text.lower() == '//':  # '/continue'
-    bot.send_message(6921647429, "Opend the DataBase  //")
-    closed = False
+#   if message.text.lower() == '//':  # '/continue'
+#     bot.send_message(6921647429, "Opend the DataBase  //")
+#     closed = False
 
-  elif message.text.lower() != '' and not closed:
-    print(message.text)
-    with open("Data_Base/File_DataBase.txt", "a", encoding="utf-8") as file:
-      file.write("\n")
-      file.write(message.text)
-#===============0 Save Telegram DataBase 0================
+#   elif message.text.lower() != '' and not closed:
+#     print(message.text)
+#     with open("Data_Base/File_DataBase.txt", "a", encoding="utf-8") as file:
+#       file.write("\n")
+#       file.write(message.text)
+# #===============0 Save Telegram DataBase 0================
 
-bot.polling(none_stop=True)
+# bot.polling(none_stop=True)
